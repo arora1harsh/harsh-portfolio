47b4a716f90fbfca7644800c748b5ab0924cd2b3
Harsh Arora <arora1harsh@gmail.com>
2026-02-14 15:48:18 +0530

    Fixed contact API to use production backend URL

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -7,13 +7,15 @@ router.post("/", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
-      },
+    },
     });
-
+    
     await transporter.sendMail({
       from: email,
       to: process.env.EMAIL_USER,
