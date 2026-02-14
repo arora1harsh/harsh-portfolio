9d6b9c183dbf86e041fcbe7b9b6ea99d0fd35aba
Harsh Arora <arora1harsh@gmail.com>
2026-02-14 11:55:08 +0530

    Added JWT auth middleware and protected project routes

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+    console.log("Auth middleware triggered");
+
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.adminId = decoded.id;
+    next();
+
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = authMiddleware;
--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Project = require("../models/Project");
+const authMiddleware = require("../middleware/authMiddleware");
+
 
 // GET all projects
 router.get("/", async (req, res) => {
@@ -13,7 +15,7 @@ router.get("/", async (req, res) => {
 });
 
 // POST new project
-router.post("/", async (req, res) => {
+router.post("/", authMiddleware, async (req, res) => {
   try {
     const newProject = new Project(req.body);
     const savedProject = await newProject.save();
@@ -22,5 +24,15 @@ router.post("/", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
+router.delete("/:id", authMiddleware, async (req, res) => {
+  try {
+    await Project.findByIdAndDelete(req.params.id);
+    res.json({ message: "Project deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 
 module.exports = router;
