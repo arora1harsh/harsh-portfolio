fadb9a30cd25dcc85254223c5a39ffa6582ec756
Harsh Arora <arora1harsh@gmail.com>
2026-02-14 13:23:03 +0530

    Updated API URLs for production deployment

--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -12,7 +12,7 @@ function AdminLogin() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/admin/login",
+        "https://harsh-portfolio-flpe.onrender.com/api/admin/login",
         { email, password }
       );
 
